432759b09433b8a312367d779ee005690658e439
Maxine <maxinecsc@gmail.com>
2018-07-17 15:26:52 -0700

    current/old author and difference < short_enough not working as expected

--- src/botbot2.py
+++ src/botbot2.py
@@ -1,5 +1,6 @@
 from disco.bot import Bot, Plugin
 import json, re
+from datetime import datetime, timedelta
 
 # start this by:
 #    python -m disco.cli --token=<YOUR TOKEN> --run-bot --plugin botbot2
@@ -7,30 +8,55 @@ import json, re
 class SimplePlugin(Plugin):
 
     known_channels = []
+    site_numbers = []
 
     # Plugins provide an easy interface for listening to Discord events
     @Plugin.listen('ChannelCreate')
     def on_channel_create(self, event):
         channel_name = event.id
-        if not (channel_name in known_channels):
-            known_channels.push(channel_name)
+        if not (channel_name in self.known_channels):
+            self.known_channels.append(channel_name)
             event.channel.send_message('Woah, a new channel huh! ' + str(channel_name))
 
     @Plugin.listen('MessageCreate')
     def on_message_create(self, event):
+        authorid = str(event.message.author)
+        author,_ = str(event.message.author).split('#')
         #self.log.debug('Got message: %s', event.message)
         #self.log.info('Got message: %s', event.message.content)
         pattern = re.compile('hi[ ]*[!]*$',re.I)
         if pattern.match(event.message.content):
            #self.log.info('Got message: %s', event.message.content)
-           event.channel.send_message('Yes? What do you want??')
+           event.channel.send_message('Hello ' + author + ', what can I do for you?')
         pattern2 = re.compile(':[-]*[\(\)\{\}]')
         #self.log.info('Got message: %s', event.message.content)
         if pattern2.match(event.message.content):
            event.channel.send_message('EMOJI ALARM!')
         pattern3 = re.compile('what[\']*[ i]*s the number?',re.I)
         if pattern3.match(event.message.content):
-           event.channel.send_message('Which site would you like the number of?')
+           event.channel.send_message('Hi ' + author + ', which site would you like the number of?')
+           self.site_numbers.append( [author, datetime.now()  ] )
+           #event.channel.send_message(json.dumps(self.site_numbers))
+        # now react to a message right after the first message
+        # get the current date and time
+        current_date = datetime.now()
+        # get the current author
+        current_author = author
+        # loop through the list of site_numbers
+        for request in self.site_numbers:
+            #   calculate the time difference in seconds between current date and site_numbers date (see datetime.timedelta)
+            old_date = request[1]
+            old_author = request[0]
+            difference = (current_date - old_date).total_seconds()
+            #   define short_enough (10sec)
+            short_enough = 10
+
+            if(current_author == old_author) and (difference < short_enough):
+                event.channel.send_message("suh dude")
+        #   if the author of the current message is the same as the current author
+        #      and if the time difference between old message and new is short enough
+        #          print result
+        #          remove message from site_numbers
 
 
     # Serious Commands
